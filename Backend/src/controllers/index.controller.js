fa96f440c3332d571663077f352a716070c35fcb
Armando5347 <masterchiefgama123@gmail.com>
2024-04-19 16:07:29 -0600

    bd de clever cloud

--- Backend/src/controllers/index.controller.js
+++ Backend/src/controllers/index.controller.js
@@ -1,6 +1,6 @@
 import { pool } from "../db.js";
 
 export const pong = async (req, res) => {
-  const result = await pool.query("SELECT 'pong' AS result");
+  const result = await pool.query("SELECT * FROM documento");
   res.json(result[0]);
 };
