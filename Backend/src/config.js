6ad74ed2c1a96bd4d1bf2f37cdf7f6d025d69596
Armando5347 <masterchiefgama123@gmail.com>
2024-04-19 16:25:11 -0600

    pequeño ajuste conexion con CLEVER CLOUD

--- Backend/src/config.js
+++ Backend/src/config.js
@@ -4,8 +4,8 @@ config();
 
 export const PORT = process.env.PORT || 3000;
 
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASS = process.env.DB_PASS || "1234";
-export const DB_DATABASE = process.env.DB_DATABASE || "hackhive";
+export const DB_HOST = process.env.DB_HOST || "bhdrlvvyh7fsns4hcjae-mysql.services.clever-cloud.com";
+export const DB_USER = process.env.DB_USER || "uphk7tztoaho2c6l";
+export const DB_PASS = process.env.DB_PASS || "TNTKfGBKhvbfeDJCHs66";
+export const DB_DATABASE = process.env.DB_DATABASE || "bhdrlvvyh7fsns4hcjae";
 export const DB_PORT = process.env.DB_PORT || 3306;
