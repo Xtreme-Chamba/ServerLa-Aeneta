bdec9d6ba6d7002543721b0aaff691436ddcbdfb
Armando5347 <masterchiefgama123@gmail.com>
2024-04-23 22:48:37 -0600

    Ajuste base de datos y controladores correspondientes

--- Backend/src/controllers/busquedas.controller.js
+++ Backend/src/controllers/busquedas.controller.js
@@ -2,27 +2,27 @@ import { pool } from "../db.js";
 
 //le pide al cuerpo de la petición un campo titulo, que es el que se manda al call para obtener el resultado
 export const postBusquedaTituloSimple = async (req, res) => {
-  const { titulo } = req.body;
-  const result = await pool.query("call busqueda_titulo_sencilla(?)", [
-    titulo
+  const { titulo, numResultados, pagina  } = req.body;
+  const result = await pool.query("call busqueda_titulo_sencilla(?,?,?)", [
+    titulo, numResultados, pagina 
   ]);
   res.json(result[0][0]);
 };
 
 //le pide al cuarpo de la petición un campo autor, que es con el que manda al call para obtener un resultado
 export const postBusquedaAutorSimple = async (req, res) => {
-    const { autor } = req.body;
-    const result = await pool.query("call busqueda_autor_sencilla(?)", [
-      autor
+    const { autor, numResultados, pagina } = req.body;
+    const result = await pool.query("call busqueda_autor_sencilla(?,?,?)", [
+      autor, numResultados, pagina 
     ]);
     res.json(result[0][0]);
   };
   
 //le pide al body un año, y este hace el call para obtener el resultado de la busqueda
 export const postBusquedaAnioPuntualSimple = async (req, res) => {
-  const { anio } = req.body;
-  const result = await pool.query("call busqueda_anio_puntual_sencilla(?)", [
-    anio
+  const { anio, numResultados, pagina  } = req.body;
+  const result = await pool.query("call busqueda_anio_puntual_sencilla(?,?,?)", [
+    anio, numResultados, pagina 
   ]);
   res.json(result[0][0]);
 };
@@ -30,9 +30,9 @@ export const postBusquedaAnioPuntualSimple = async (req, res) => {
 //le pide al cuarpo de la petición los campos de año de inicio y fin, para hacer el 
 //call y obtener un resultado de búsqueda
 export const postBusquedaAnioRangoSimple = async (req, res) => {
-  const { anio_inicio, anio_final } = req.body;
-  const result = await pool.query("call busqueda_anio_rengo_sencilla(?,?)", [
-    anio_inicio,anio_final
+  const { anio_inicio, anio_final, numResultados, pagina  } = req.body;
+  const result = await pool.query("call busqueda_anio_rengo_sencilla(?,?,?,?)", [
+    anio_inicio,anio_final, numResultados, pagina 
   ]);
   res.json(result[0][0]);
 };
