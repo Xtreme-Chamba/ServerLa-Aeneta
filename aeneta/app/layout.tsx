bcd056bf1279dea3f4b01bf1c52007977ba79cd6
Armando5347 <masterchiefgama123@gmail.com>
2024-06-19 15:20:31 -0600

    navbar

--- aeneta/app/layout.tsx
+++ aeneta/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import BarraNavegacion from "./componentes/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className ={inter.className + " bg-fondo"}  >
-        {/* Navbar */}
+        <BarraNavegacion />
         {children}
         {/*Footer */}
         </body>
